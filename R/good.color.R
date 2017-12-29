dbabafac79852e1c8b7cb5bb7f8b24324b36d90b
Timothy R. Koscik <timkoscik@gmail.com>
2017-12-29 12:01:42 -0600

    Update good.color.R

--- R/good.color.R
+++ R/good.color.R
@@ -7,20 +7,20 @@ good.color <- function(type = c("cool", "warm", "neon", "neon-bright"), n=18) {
       c(colorRampPalette(c("#0F154B", "#1a237e"))(3),
         colorRampPalette(c("#1a237e", "#42b3d5"))(7)[-1],
         colorRampPalette(c("#42b3d5", "#dcedc8"))(5)[-1],
-        colorRampPalette(c("#dcedc8", "#ffffff"))(6)[-1]))(n)
+        colorRampPalette(c("#dcedc8", "#eaf4de"))(3)[-1]))(n)
   } else if (type == "warm") {
     colors <- colorRampPalette(
       c(colorRampPalette(c("#4d342f", "#e4521b"))(5),
         colorRampPalette(c("#e4521b", "#feeb65"))(8)[-1],
-        colorRampPalette(c("#feeb65", "#ffffff"))(7)[-1]))(n)
+        colorRampPalette(c("#feeb65", "#fef4b2"))(4)[-1]))(n)
   } else if (type == "neon") {
     colors <- colorRampPalette(
       c(colorRampPalette(c("#2D0B41", "#6a1b9a"))(5),
         colorRampPalette(c("#6a1b9a", "#e85285"))(5)[-1],
         colorRampPalette(c("#e85285", "#ffecb3"))(6)[-1],
-        colorRampPalette(c("#ffecb3", "#ffffff"))(5)[-1]))(n)
+        colorRampPalette(c("#ffecb3", "#fff0c5"))(5)[-1]))(n)
   } else if (type == "neon-bright") {
-    colors <- colorRampPalette(c("#000000", "#0000ff", "#ff00ff", "#ffff00", "#ffffff"))(n)
+    colors <- colorRampPalette("#000000", "#0000ff", "#ff00ff", "#ffff00", "#ffffff")(n)
   }
   return(colors)
 }

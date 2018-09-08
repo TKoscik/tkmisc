9e837b1033d782560e5740c75c49ae04aad24aa9
Timothy R. Koscik <timkoscik@gmail.com>
2018-09-08 10:44:34 -0500

    Update center.var.R

--- R/center.var.R
+++ R/center.var.R
@@ -1,13 +1,16 @@
 center.var <- function(df, to.scale, scale.by) {
   group.var <- unlist(df[scale.by])
   groups <- unlist(unique(df[scale.by]))
-  ctr <- numeric()
-  m <- numeric()
+  ctr <- numeric(nrow(df))
+  m <- numeric(nrow(df))
   for (i in 1:length(groups)) {
-    tf <- df[group.var==groups[i], ]
+    group.idx <- which(group.var==groups[i])
+    tf <- df[group.idx, ]
     m.temp <- mean(unlist(tf[to.scale]))
-    ctr <- c(ctr, (unname(unlist(tf[to.scale])-m.temp)))
-    m <- c(m, rep(m.temp, nrow(tf)))
+    ctr[group.idx] <- (unname(unlist(tf[to.scale])-m.temp))
+    m[group.idx] <- rep(m.temp, nrow(tf))
+    #ctr <- c(ctr, (unname(unlist(tf[to.scale])-m.temp)))
+    #m <- c(m, rep(m.temp, nrow(tf)))
   }
   df[paste0(to.scale, ".ctr")] <- ctr
   df[paste0(to.scale, ".mean")] <- m

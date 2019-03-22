4d6740fa6b4b6cd8730d52690090c6089f6ea55d
Timothy R. Koscik <timkoscik@gmail.com>
2019-03-22 09:30:14 -0500

    Update timbow.R

--- R/timbow.R
+++ R/timbow.R
@@ -1,5 +1,5 @@
 timbow <- function(n=10, reverse=F) {
-  x <- colorRampPalette(c(viridis(5), viridis(5, begin=0.5, direction = -1, option="plasma")[-1])
+  x <- colorRampPalette(c(viridis(5), viridis(5, begin=0.5, direction = -1, option="plasma")[-1]))
   output <- x(n)
   if (reverse) {
     output <- rev(output)
